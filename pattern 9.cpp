0ff57cc7eabc32286958fe2f150f8090cffb3b06
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-21 19:05:19 +0530

    Add files via upload

---
+++ pattern 9.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter a number:";
+	cin>>n;
+	for(int i=n;i>=1;i--){
+		for(int k=n-i;k>0;k--){
+			cout<<" ";
+		}
+		for(int j=i;j>0;j--){
+			cout<<"* ";
+		}
+		cout<<"\n";
+	}
+}
