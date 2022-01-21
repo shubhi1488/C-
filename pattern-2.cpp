7f94aa6c6de4c448a262e18a7ca8d967bd45d5ee
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-21 13:01:44 +0530

    Add files via upload

---
+++ pattern-2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void HalfPyramid(int n){
+	for(int i=1;i<=n;i++){
+		for(int k=n-i;k>0;k--){
+			cout<<" ";
+		}
+		for(int j=1;j<=i;j++){
+		    cout<<"*";
+			}
+			
+		
+		cout<<"\n";
+	}
+}
+int main(){
+	int n;
+	cout<<"enter a number:"<<"\n";
+	cin>>n;
+	HalfPyramid(n);
+	return 0;
+}
