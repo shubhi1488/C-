63b4faad498a268bccc5a8b85d262c3d698eb05a
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-22 19:29:40 +0530

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void HalfPyramid(int n){
+	char alphabet='a';
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<alphabet++;
+		}
+		cout<<"\n";
+	}
+}
+int main(){
+	int n;
+	cout<<"enter a number";
+	cin>>n;
+	HalfPyramid(n);
+	cout<<"\n";
+	return 0;
+}
