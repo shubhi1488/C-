a768f3497b8b3889159f47cb784e49825aecd646
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-23 14:52:35 +0530

    Add files via upload

---
+++ hollow diamond.cpp
@@ -0,0 +1,46 @@
+//hollow pattern
+#include<iostream>
+using namespace std;
+void hollowDiamond(int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<(2*n);j++){
+			if(i+j<=n-1){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+			}
+			if((i+n)<=j){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<(2*n);j++){
+			if(i>=j){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+			}
+			if(i>=(2*n-1)-j){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+			}
+		}
+		cout<<"\n";
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+	hollowDiamond(n);
+	cout<<"\n";
+	return 0;
+}
